22456e2c97b87d059c32293196b914791f542b0e
sblaurock <sblaurock@gmail.com>
2013-11-04 00:59:09 -0500

    Moved JS into module. Progress in the direction of connecting two peers.

--- include/js/script.js
+++ include/js/script.js
@@ -1,49 +1,96 @@
-$(document).ready(function() {
-	var options = {
+var bytecast = function() {
+	var _options = {
 		dropArea: {
 			reference: $('#drop'),
 			hoverClass: 'hover',
 			activeClass: 'active'
+		},
+		copy: {
+			initializing: 'Creating session...',
+			created: 'Session created. Share this link:<br />',
+			connecting: 'Connecting to peer...'
 		}
 	};
 
-	var bindDropEvents = function() {
-		var toggleActiveStop = function(e) {
-			options.dropArea.reference.toggleClass(options.dropArea.hoverClass);
-			stopEvent(e);
-		};
+	var _session = {
+		id: '',
+		file: null,
+		peer: null
+	};
 
-		var stopEvent = function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-		};
+	return {
+		// Handle events around drag and drop functionality.
+		bindDropEvents: function() {
+			var instance = this;
 
-		options.dropArea.reference.on('dragover', stopEvent);
-		options.dropArea.reference.on('dragenter', toggleActiveStop);
-		options.dropArea.reference.on('dragleave', toggleActiveStop);
+			var toggleActiveStopEvent = function(e) {
+				_options.dropArea.reference.toggleClass(_options.dropArea.hoverClass);
+				stopEvent(e);
+			};
 
-		options.dropArea.reference.on('dragenter', function(e) {
-			e.preventDefault();
-			e.stopPropagation();
-		});
+			var stopEvent = function(e) {
+				e.preventDefault();
+				e.stopPropagation();
+			};
 
-		options.dropArea.reference.on('drop', function(e) {
-			if(e.originalEvent.dataTransfer) {
-				if(e.originalEvent.dataTransfer.files.length) {
-					var file = e.originalEvent.dataTransfer.files[0];
+			_options.dropArea.reference.on('dragover', stopEvent);
+			_options.dropArea.reference.on('dragenter', toggleActiveStopEvent);
+			_options.dropArea.reference.on('dragleave', toggleActiveStopEvent);
 
-					options.dropArea.reference.html('Gotcha!');
-					stopEvent(e);
+			_options.dropArea.reference.on('drop', function(e) {
+				if(e.originalEvent.dataTransfer) {
+					if(e.originalEvent.dataTransfer.files.length) {
+						_session.file = e.originalEvent.dataTransfer.files[0];
+						toggleActiveStopEvent(e);
+
+						instance.initiateSession(function() {
+							_options.dropArea.reference.html(_options.copy.created + document.URL + '#' + id);
+							instance.listenForPeer();
+						});
+					}
 				}
-			}
-		});
-	}();
+			});
+		},
 
-	var initiatePeerSession = function() {
-		var peer = new Peer({key: '4u478wggtkxzuxr'});
+		// Create a peer session.
+		initiateSession: function(callback) {
+			var instance = this;
+			var peer = new Peer({key: '4u478wggtkxzuxr'});
 
-		peer.on('open', function(id) {
-			console.log('My peer ID is: ' + id);
-		});
-	}();
+			_options.dropArea.reference.html(_options.copy.initializing);
+
+			peer.on('open', function(id) {
+				_session.id = id;
+				_session.peer = peer;
+
+				callback(id);
+			});
+		},
+
+		// Listen for incoming connections.
+		listenForPeer: function() {
+			_session.peer.on('connection', function(c) {
+				console.log('Connection established.');
+			});
+		},
+
+		// Connect to peer if we have a hash.
+		connectToPeer: function(peerId) {
+			this.initiateSession(function(id) {
+				_options.dropArea.reference.html(_options.copy.connecting);
+				console.log(peerId, id);
+			});
+		}
+	};
+}();
+
+// Kick out the jams.
+$(document).ready(function() {
+	var hash = window.location.hash;
+
+	if(hash && typeof hash === 'string' && hash.length > 0) {
+		bytecast.connectToPeer(hash.replace('#', ''));
+	} else {
+		bytecast.bindDropEvents();
+	}
 });
