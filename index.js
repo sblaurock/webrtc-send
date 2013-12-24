348ccf7a9e7ade4d746200a1fd709893edb39577
Steve Blaurock <sblaurock@gmail.com>
2013-12-24 01:06:58 -0500

    Forgot to add index.js update.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
+var port = process.env.PORT || 5000;
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.logger());
 
-app.listen(80);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
